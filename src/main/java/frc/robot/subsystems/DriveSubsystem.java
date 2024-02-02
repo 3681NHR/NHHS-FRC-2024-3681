0cd2d8a6e92e12df49ac54d43b86110ca77102d9
potatinator <potatinator@proton.me>
2024-02-01 16:33:30 -0800

    vendordeps added and imports fixed

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,8 +4,8 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import frc.robot.Drive;
 import frc.robot.Constants;
