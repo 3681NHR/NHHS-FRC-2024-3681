9bea9bd24491b736597cc0b1dea489ccc7106c00
rob0cat <sam@murkylabs.com>
2024-02-05 17:37:08 -0800

    stuff for intake

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -6,32 +6,40 @@ package frc.robot.subsystems;
 
 // import com.revrobotics.CANSparkMax.IdleMode;
 
-import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import frc.robot.wrappers.VictorWrapper;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.Command;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.enums.IntakeState;
 import frc.robot.enums.IntakeSwingState;
-import frc.robot.wrappers.SparkWrapper;
+import frc.robot.Constants;
 
 public class IntakeSubsystem extends SubsystemBase {
 
-  private VictorWrapper m_intake;
+  private VictorWrapper m_intakeBottom;
+  private VictorWrapper m_intakeTop;
   private VictorWrapper m_rotate;
   
   private IntakeState state = IntakeState.IDLE;
   private IntakeSwingState swingState = IntakeSwingState.DOWN;
 
-
+  private Encoder intakeEncoder = new Encoder(Constants.INTAKE_SWING_ENCODER_DIO_PIN_A, Constants.INTAKE_SWING_ENCODER_DIO_PIN_B);
+  private DigitalInput homingSwitch = new DigitalInput(Constants.INTAKE_SWING_LIMIT_SWITCH_DIO_PIN);
+  private PIDController swingPID = new PIDController(0, 0, 0);
   
-  private final double INTAKE_SPEED = 0.1;
-  private final double ROTATE_SPEED = 0.1;
+  private double selectedPosition;
+  
+
 
   /** Creates a new Subsystem. */
   public IntakeSubsystem() {
-   this.m_intake = new VictorWrapper(1);
-   this.m_rotate = new VictorWrapper(1);
+   this.m_intakeBottom = new VictorWrapper(Constants.INTAKE_BOTTOM_MOTOR_ID);
+   this.m_intakeTop = new VictorWrapper(Constants.INTAKE_TOP_MOTOR_ID);
+   this.m_rotate = new VictorWrapper(Constants.INTAKE_SWING_MOTOR_ID);
    
   }
 
