d946d23d94bc1ce5829d1e2d55ccc73eb63f15d1
potatinator <potatinator@proton.me>
2024-04-05 10:53:03 -0700

    changes

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,14 +1,8 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.Degree;
 import static edu.wpi.first.units.Units.Radian;
 import static edu.wpi.first.units.Units.Rotation;
-
-// import com.revrobotics.CANSparkMax.IdleMode;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
@@ -17,16 +11,13 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.units.Angle;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.MutableMeasure;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.enums.IntakeState;
 import frc.robot.enums.IntakeSwingState;
 import frc.robot.Constants;
 
@@ -34,13 +25,12 @@ public class IntakeSubsystem extends SubsystemBase {
 
    private VictorSPX m_intakeBottom = new VictorSPX(Constants.INTAKE.BOTTOM_MOTOR_ID);
    private VictorSPX m_intakeTop    = new VictorSPX(Constants.INTAKE.TOP_MOTOR_ID   );
-   private CANSparkMax m_rotate       = new CANSparkMax(Constants.INTAKE_SWING.MOTOR_ID, MotorType.kBrushless);
+   private CANSparkMax m_rotate     = new CANSparkMax(Constants.INTAKE_SWING.MOTOR_ID, MotorType.kBrushless);
 
    private DigitalInput holdSwitch = new DigitalInput(Constants.INTAKE.DETECTOR_DIO_PIN);
 
    private double pidOut = 0.0;
   
-  private IntakeState state = IntakeState.IDLE;
   private IntakeSwingState swingState = IntakeSwingState.IDLE;
 
   private DutyCycleEncoder intakeSwingEncoder = new DutyCycleEncoder(Constants.INTAKE_SWING.ENCODER_DIO_PIN);
@@ -56,15 +46,18 @@ public class IntakeSubsystem extends SubsystemBase {
   Constants.INTAKE_SWING.P_GAIN,
   Constants.INTAKE_SWING.I_GAIN,
   Constants.INTAKE_SWING.D_GAIN,
-  new Constraints(10, 20)
+  null
   );
-  //pids are for nerds like me
+
   private Measure<Angle> selectedAngle;
 
-  /** Creates a new Subsystem. */
   public IntakeSubsystem() {
 
    //set motor idle modes
+   this.m_intakeBottom.setNeutralMode(NeutralMode.Brake);
+   this.m_intakeTop   .setNeutralMode(NeutralMode.Brake);
+   this.m_rotate      .setIdleMode(IdleMode.kBrake);
+
   
   }
 
@@ -93,13 +86,13 @@ public class IntakeSubsystem extends SubsystemBase {
   }
 
   public String getSwingState(){return swingState.toString();}
-  public String getState() {return state.toString();}
   public boolean getSwitchEnabled(){return switchEnabled;}
   public void setSwitchEnabled(boolean enabled){switchEnabled = enabled;}
  
   @Override
   public void periodic() {
 
+    //failsafe changes up and down positions when encoder jumps a phase, no clue why it doeds this 
     if(intakeSwingEncoder.getDistance() < Constants.INTAKE_SWING.PID_SWITCH.in(Radian)){
       upPos  = (MutableMeasure<Angle>) Constants.INTAKE_SWING.UP_POSITION.minus(Rotation.of(1));
       downPos = (MutableMeasure<Angle>) Constants.INTAKE_SWING.DOWN_POSITION.minus(Rotation.of(1));
@@ -115,20 +108,10 @@ public class IntakeSubsystem extends SubsystemBase {
 
     holding = !holdSwitch.get();
       
-   this.m_intakeBottom.setNeutralMode(NeutralMode.Brake);
-   this.m_intakeTop   .setNeutralMode(NeutralMode.Brake);
-   this.m_rotate      .setIdleMode(IdleMode.kBrake);
-
     if(swingState == IntakeSwingState.UP){
       selectedAngle = upPos;
     }
-    if(swingState == IntakeSwingState.DOWN){
-      selectedAngle = downPos;
-      if(holding && switchEnabled){
-        swingState = IntakeSwingState.UP;
-      }
-    }
-    if(swingState == IntakeSwingState.DOWN_HOLDING){
+    if(swingState == IntakeSwingState.DOWN || swingState == IntakeSwingState.DOWN_HOLDING){
       selectedAngle = downPos;
     }
     if(swingState == IntakeSwingState.IDLE){
@@ -140,27 +123,6 @@ public class IntakeSubsystem extends SubsystemBase {
 
     m_rotate.set(pidOut);
 
-
-  if(state == IntakeState.INTAKE){
-    if(!holding || !switchEnabled){
-      m_intakeBottom.set(ControlMode.PercentOutput, Constants.INTAKE.INTAKE_OUTPUT);
-      m_intakeTop   .set(ControlMode.PercentOutput, Constants.INTAKE.INTAKE_OUTPUT);
-    } else {
-      setIntake(IntakeState.IDLE);
-    }
-  }
-  if(state == IntakeState.COMPRESS){
-    m_intakeBottom.set(ControlMode.PercentOutput, Constants.INTAKE.INTAKE_OUTPUT);
-    m_intakeTop   .set(ControlMode.PercentOutput, Constants.INTAKE.INTAKE_OUTPUT);
-  }
-  if(state == IntakeState.REVERSE){
-    m_intakeBottom.set(ControlMode.PercentOutput, Constants.INTAKE.REVERSE_OUTPUT);
-    m_intakeTop   .set(ControlMode.PercentOutput, Constants.INTAKE.REVERSE_OUTPUT);    
-  } 
-  if(state == IntakeState.IDLE){
-    m_intakeBottom.set(ControlMode.PercentOutput, 0);
-    m_intakeTop   .set(ControlMode.PercentOutput, 0);
-  }
 }
   
   private double clamp(double val, double min, double max)  {
@@ -174,7 +136,6 @@ public class IntakeSubsystem extends SubsystemBase {
     builder.addDoubleProperty("intake top roller out"   , m_intakeTop::getMotorOutputPercent   , null);
     builder.addDoubleProperty("intake bottom roller out", m_intakeBottom::getMotorOutputPercent, null);
     builder.addStringProperty("intake swing state"      , this::getSwingState                  , null);
-    builder.addStringProperty("intake state"            , this::getState                       , null);
     builder.addBooleanProperty("intake switch"          , this::isHolding                      , null);
     builder.addBooleanProperty("intake switch enabled"  , this::getSwitchEnabled               , this::setSwitchEnabled);
   }
